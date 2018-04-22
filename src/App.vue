6688414df7be3ec6ad96a7cd3661b09facaf5129
d.radkovskiy <d.radkovskiy@crazypanda.ru>
2018-04-22 17:42:59 +0300

    format this shit

--- src/App.vue
+++ src/App.vue
@@ -16,68 +16,68 @@ import BetsList from './bets/List.vue'
 import CreateBet from './create/Create.vue'
 
 export default Vue.extend({
-  data() {
-    return {
-      modeCreate: window.location.pathname == "/create",
-      loading: false,
-      create: {
-        currency: "Bitcoin",
-      }
-    }
-  },
-  methods: {
-    createBet(form: {cur: "Ethereum" | "Bitcoin" | "Litecoin" | "Ripple", cond: string, price1: number, price2: number, date: number})
-    {
-      const SHORT = {
-        Ethereum: "ETH",
-        Bitcoin: "BTC",
-        Litecoin: "LTC",
-        Ripple: "XRP",
-      }
+	data() {
+		return {
+			modeCreate: window.location.pathname == "/create",
+			loading: false,
+			create: {
+				currency: "Bitcoin",
+			}
+		}
+	},
+	methods: {
+		createBet(form: {cur: "Ethereum" | "Bitcoin" | "Litecoin" | "Ripple", cond: string, price1: number, price2: number, date: number})
+		{
+			const SHORT = {
+				Ethereum: "ETH",
+				Bitcoin: "BTC",
+				Litecoin: "LTC",
+				Ripple: "XRP",
+			}
 
-      console.log(`create bet: ${JSON.stringify(form)}`)
+			console.log(`create bet: ${JSON.stringify(form)}`)
 
-      fetch('/create_bet', {
-          method: 'POST',
-          body: JSON.stringify({
-            bet_info: {
-              for_amount: form.price1,
-              value: form.price2,
-              currency: SHORT[form.cur],
-              datetime: form.date,
-            },
-            user_info: { email: "test_user@mail.ru" }
-          }),
-          headers: new Headers({
-            'Content-Type': 'application/json'
-          })
-      }).then(() => { window.location.href = '/' })
-      /*
-      {
-      bet_info: {for_amount: 1000, value: 50, currency: "ETH"},
-      user_info: {email: "unique id"}
-      }
-      */
-    },
-    listBetClick(bet: {})
-    {
-      console.log(`bet clicked: ${JSON.stringify(bet)}`)
-    }
-  },
-  mounted: function ()
-  {
-    console.log()
-    let _this = this
-    setTimeout(() => {
-      _this.loading = false
-    }, 1000)
-  },
-  components: {
-    AppHeader,
-    AppFooter,
-    BetsList,
-    CreateBet,
-  }
+			fetch('/create_bet', {
+				method: 'POST',
+				body: JSON.stringify({
+					bet_info: {
+						for_amount: form.price1,
+						value: form.price2,
+						currency: SHORT[form.cur],
+						datetime: form.date,
+					},
+					user_info: { email: "test_user@mail.ru" }
+				}),
+				headers: new Headers({
+					'Content-Type': 'application/json'
+				})
+			}).then(() => { window.location.href = '/' })
+			/*
+			{
+			bet_info: {for_amount: 1000, value: 50, currency: "ETH"},
+			user_info: {email: "unique id"}
+			}
+			*/
+		},
+		listBetClick(bet: {})
+		{
+			console.log(`bet clicked: ${JSON.stringify(bet)}`)
+		}
+	},
+	mounted: function ()
+	{
+		console.log()
+		let _this = this
+		setTimeout(() => {
+			_this.loading = false
+		}, 1000)
+	},
+	components: {
+		AppHeader,
+		AppFooter,
+		BetsList,
+		CreateBet,
+	}
 })
 </script>
 
