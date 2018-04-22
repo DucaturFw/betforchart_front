8dee81f13e824a6663ebe0d7b6bbf48c38dcb296
d.radkovskiy <d.radkovskiy@crazypanda.ru>
2018-04-22 15:25:34 +0300

    bet list styles

--- src/bets/Bet.vue
+++ src/bets/Bet.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="bet-entry">
     <span class="text-main">{{ bet.cur }} at ${{ bet.price }} on {{ bet.date }}</span>
-    <input v-if="complete" class="btn" type="button" value="Bet">
+    <input v-if="!complete" class="btn" type="button" value="Bet">
+    <img class="check" v-if="complete" src="https://a.icons8.com/bbpXpmkk/0iOHYM/btnnormal.png" alt="V">
   </div>
 </template>
 
@@ -29,13 +30,31 @@ span.text-main {
     width: 80%;
 }
 input.btn {
-    width: 20%;
+  /* width: 20%; */
+  border: none;
+  border-radius: 25px;
+  width: 149px;
+  height: 36px;
+  background: #ff1052;
+  color: white;
+  font-size: 18px;
+  font-weight: 700;
+  margin-top: -7px;
+  float: right;
+  cursor: pointer;
 }
+
+img.check {
+  margin-top: -7px;
+  float: right;
+}
+
 div.bet-entry {
   border-radius: 8px;
   /* width: 770px; */
   /* height: 68px; */
   background: #ffffff;
+  font-family: Montserrat, "Open Sans", Helvetica, Arial, sans-serif;
   color: #211f51;
   box-shadow: 0px 1px 3px rgba(32,32,38,0.12);
   padding: 20px;
